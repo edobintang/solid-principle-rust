62f79b6c4badd36f217acde115c7184f3171f7ae
domain121314 <telkomdev.domain@gmail.com>
2023-10-30 20:07:07 +0700

    first init project

---
+++ src/main.rs
@@ -0,0 +1,5 @@
+pub mod principles;
+
+fn main() {
+    println!("Hello, world!");
+}
---
+++ src/principles/mod.rs
@@ -0,0 +1,5 @@
+pub mod single_responsibility;
+pub mod open_closed;
+pub mod liskov_subtitution;
+pub mod interface_segregation;
+pub mod dependency_injection;
